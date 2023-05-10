1d3e97aa88863d7500e0048af70908ef66868934
Jesus Molina <netod90@gmail.com>
2023-05-09 21:38:00 -0700

    Add redux initial set up

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import bookReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
+
+export const store = configureStore({
+  reducer: {
+    book: bookReducer,
+    categories: categoriesReducer,
+  },
+});
