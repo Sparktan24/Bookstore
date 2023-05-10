e61569821da12557d449ae61617fbd6c1fab3900
Jesus Molina <netod90@gmail.com>
2023-05-09 21:45:02 -0700

    Fix linter

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/booksSlice';
 import categoriesReducer from './categories/categoriesSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     book: bookReducer,
     categories: categoriesReducer,
   },
 });
+
+export default store;
