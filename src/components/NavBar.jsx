82b093d473c0f8f6acc57351617771b3ccdec178
Jesus Molina <netod90@gmail.com>
2023-05-13 18:05:03 -0700

    Add navBar styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
-import styles from '../styles/NavBar.module.css';
+import '../styles/navBar.css';
+//  import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => (
-  <nav className={styles.nav}>
-    <h1>Bookstore CMS</h1>
-    <ul>
-      <li>
-        <Link to="/">Books</Link>
+  <nav className="navCont">
+    <h1 className="navTitle">Bookstore CMS</h1>
+    <ul className="navUl">
+      <li className="navLi">
+        <Link to="/" activeclassname="active">Books</Link>
       </li>
-      <li>
-        <Link to="categories">Categories</Link>
+      <li className="navLi">
+        <Link to="categories" activeclassname="active">Categories</Link>
       </li>
     </ul>
   </nav>
